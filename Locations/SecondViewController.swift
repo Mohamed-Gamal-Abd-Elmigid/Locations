45903055262f29c8f5bce9fe860dd30579f03088
mohamedgit config --global user.namemohamed-gamal <mohamed gamal>
2020-07-09 14:42:20 +0200

    Initial Commit

---
+++ Locations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Locations
+//
+//  Created by mac on 7/9/20.
+//  Copyright © 2020 M.gamal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
